422b133f4286a9673bef27343a8a3dc3bb326b94
danielpabalate <122656027+danielpabalate@users.noreply.github.com>
2025-03-06 08:32:59 +0800

    Minor edit

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,11 +3,11 @@
 @section('content')
 
     @if (Auth::user()->role->role_name == 'SuperAdmin')
-        I'm super admin
+        
     @elseif (Auth::user()->role->role_name == 'Admin') 
-        I'm admin
+        
     @elseif (Auth::user()->role->role_name == 'Rider')
-        I'm super rider
+        
     @endif
 
 @endsection
--- resources/views/include/sidebar-menu/order.blade.php
+++ resources/views/include/sidebar-menu/order.blade.php
@@ -1,7 +1,7 @@
 {{-- Order --}}
 <li class="nav-item">
     <a href="{{route('order.index')}}" class="nav-link {{ Str::contains(Request::url(), '/orders') ? 'active' : '' }}">
-        <i class="fa-solid fa-fw fa-building" style="color: #fda720;"></i>
+        <i class="fa-solid fa-fw fa-box" style="color: #fda720;"></i>
         <p style="margin-left: 2px;">Order</p>
     </a>
 </li>
